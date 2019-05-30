9f31fd85eb0f2e61f37748109c42054182991cf0
gukbong2 <junyeon2459@gmail.com>
2019-05-30 11:33:57 +0800

    20190530

--- oraclePart2/src/main/java/org/zerock/config/ServletConfig.java
+++ oraclePart2/src/main/java/org/zerock/config/ServletConfig.java
@@ -1,6 +1,11 @@
 package org.zerock.config;
 
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -9,7 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
-@ComponentScan(basePackages = { "org.zerock.controller"})
+@ComponentScan(basePackages = { "org.zerock.controller", "org.zerock.exception"})
 public class ServletConfig implements WebMvcConfigurer {
 
 	@Override
@@ -26,5 +31,28 @@ public class ServletConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver getResolver() throws IOException {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		
+		//한번의 Request의 용량 10MB 제한
+		resolver.setMaxUploadSize(1024 * 1024 * 10);
+		
+		//파일 1개당 2MB 제한
+		resolver.setMaxUploadSizePerFile(1024 * 1024 * 2);
+		
+		//메모리상에서 유지하는 최대의 크기
+		resolver.setMaxInMemorySize(1024 * 1024);
+		
+		//임시저장 경로
+		resolver.setUploadTempDir(new FileSystemResource("E:\\upload\\tmp"));
+		
+		//UTF-8 엔코딩 타입
+		resolver.setDefaultEncoding("UTF-8");
+		
+		return resolver;
+		
+		
 	}
 }
--- oraclePart2/src/main/java/org/zerock/config/WebConfig.java
+++ oraclePart2/src/main/java/org/zerock/config/WebConfig.java
@@ -1,5 +1,7 @@
 package org.zerock.config;
 
+import javax.servlet.ServletRegistration;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
@@ -10,13 +12,16 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		// TODO Auto-generated method stub
 		return new Class[] {ServletConfig.class};
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		// TODO Auto-generated method stub
 		return new String[] { "/" };
+	}
+	
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
 	}
 }
